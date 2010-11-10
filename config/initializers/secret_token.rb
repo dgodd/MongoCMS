dd1fcec437edb697ccfcfcaa6897baf9126e68a8
dgodd <dgodd@s2.lypc.com.au>
2010-11-10 09:04:42 +0000

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoCMS::Application.config.secret_token = '3b32cf9dfa9587a5521aaca5e3252253d90ecb7c77ab6eafc6ec1da3a614f0c9c8654df832ff3a8b24b862b6df3b1e932cb102f1d4bf2e978d7db87cb31af54b'
