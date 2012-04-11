800f050ee166a7d8b016fdfbf64f00da3b0d6e31
Dave Goddard <goddard.dave@gmail.com>
2012-04-10 20:38:31 -0400

    Make asset precompile work

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,10 @@ if ENV['MONGOHQ_URL']
   ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub('/', '')
 end
 
-require 'mongoid/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'active_resource/railtie'
 require "sprockets/railtie"
-#require 'rails/test_unit/railtie'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
