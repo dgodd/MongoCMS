1d1e81cf781d7efd3c8697e90e8bdf802775a28f
Dave Goddard <goddard.dave@gmail.com>
2012-04-26 08:51:01 -0400

    change the session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MongoCMS::Application.config.session_store :cookie_store, :key => '_MongoCMS_session'
+MongoCMS::Application.config.session_store :cookie_store, :key => '_mongocms_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
