2f03b56d1379c7687f217a93e4fb34e616f60bdb
Dave Goddard <goddard.dave@gmail.com>
2012-04-26 19:30:08 -0400

    enforce only cath login

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,28 @@
 class AdminController < ApplicationController
-	def index
-		redirect_to sites_path and return if signed_in?
+  def index
+    redirect_to sites_path and return if signed_in?
     redirect_to 'http://lypc.herokuapp.com/admin' and return unless request.host=='lypc.herokuapp.com' || request.host=='localhost'
-	end
-	def logout
-		self.current_user = nil
-		redirect_to '/admin'
-	end
-	def callback
-		auth = request.env['omniauth.auth']
-		@user = User.find_or_create_by(:provider=>auth['provider'], :uid=>auth['uid'])
-		@user.name = auth['user_info']['name'] rescue 1
-		@user.email = auth['extra']['user_hash']['email'] rescue 1
-		@user.save
-		self.current_user = @user
+  end
+  def logout
+    self.current_user = nil
+    redirect_to '/admin'
+  end
+  def callback
+    auth = request.env['omniauth.auth']
+    @user = User.find_or_create_by(:provider=>auth['provider'], :uid=>auth['uid'])
+    @user.name = auth['user_info']['name'] rescue 1
+    @user.email = auth['extra']['user_hash']['email'] rescue 1
+    @user.save
 
-    render :text=>auth.to_yaml
-  		# render :text=>"Hello, #{@user.name}"
-		# redirect_to sites_path
-	end
+    # render :text=>auth.to_yaml
+    # render :text=>"Hello, #{@user.name}"
+
+    if ['catherine@liveyourpassion.com.au', ''].include?(@user.email)
+      self.current_user = @user
+      redirect_to sites_path
+    else
+      flash[:notice] = 'Only Catherine can login'
+      redirect_to '/'
+    end
+  end
 end
