fed4124e83dea9a2b86069be823108ae3c0a17ac
Dave Goddard <goddard.dave@gmail.com>
2012-06-06 00:16:46 -0400

    use mailgun

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,9 @@
-# ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.smtp_settings = {
-  :user_name => "app2392207@heroku.com",
-  :password => "aurium",
-  :domain => "lypc.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'lypc.heroku.com',
   :authentication => :plain,
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
