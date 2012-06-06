1479e064582c8836802eb85ff3413d1efe0727b0
Dave Goddard <goddard.dave@gmail.com>
2012-06-05 23:41:29 -0400

    use sendgrid

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,13 +1,13 @@
-class ContactController < ApplicationController  
-  respond_to :html, :xml, :json  
+class ContactController < ApplicationController
+  respond_to :html, :xml, :json
 
-  def create  
+  def create
     params[:contact][:page_id] = BSON::ObjectId(params[:contact][:page_id])
-    @contact = Contact.new(params[:contact])  
-    if @contact.save  
-      flash[:notice] = "Thankyou for contacting us."  
+    @contact = Contact.new(params[:contact])
+    if @contact.save
+      flash[:notice] = "Thankyou for contacting us."
       Notifier.contact_form(@contact).deliver
-    end  
+    end
     respond_with(@contact.page)
-  end  
+  end
 end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,1 +1,10 @@
-ActionMailer::Base.delivery_method = :sendmail
+# ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app2392207@heroku.com",
+  :password => "aurium",
+  :domain => "lypc.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
